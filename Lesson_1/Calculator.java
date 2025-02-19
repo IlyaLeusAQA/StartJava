5f258fa00b49932ab238fc9b9de40e4aa59f6219
ilyaLeus <ilyaleus@gmail.com>
2025-02-19 18:07:19 +0700

    add new Calculator.java

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int operand1 = 2;
+        int operand2 = 10;
+        char operator = '^';
+        int result = 0;
+
+        if (operator == '+') {
+            result = operand1 + operand2;
+        } else if (operator == '-') {
+            result = operand1 - operand2;
+        } else if (operator == '*') {
+            result = operand1 * operand2;
+        } else if (operator == '/') {
+            result = operand1 / operand2;
+        } else if (operator == '%') {
+            result = operand1 % operand2;
+        } else {
+            result = 1;
+            for (int i = 0; i < operand2; i++) {
+                result *= operand1;
+            }
+        }
+        System.out.println(operand1 + " " + operator + " " + operand2 + " = " + result);
+    }
+}
