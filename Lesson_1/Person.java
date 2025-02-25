c91e778a5c635b56ba3a67742ef7091df0026a14
ilyaLeus <ilyaleus@gmail.com>
2025-02-25 20:12:46 +0700

    add new Person.java

---
+++ Lesson_1/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    String gender = "man";
+    String firstName = "Ivan";
+    int height = 190;
+    int weight = 100;
+    int age = 30;
+
+    public void move() {
+        System.out.println("Идти");
+    }
+
+    public void sit() {
+        System.out.println("Cидеть");
+    }
+
+    public void run() {
+        System.out.println("Бежать");
+    }
+
+    public void talk() {
+        System.out.println("Говорить");
+    }
+
+    public void learnJava() {
+        System.out.println("Учить Java");
+    }
+}
+
