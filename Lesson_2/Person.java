3d47552446a015bc8e20a708e9260b23b17c9dd7
ilyaLeus <ilyaleus@gmail.com>
2025-02-26 15:44:50 +0700

    add new folder Lesson_2 and fixed Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    String gender = "man";
+    String firstName = "Ivan";
+    float height = 190.0f;
+    float weight = 100.0f;
+    int age = 30;
+
+    public void move() {
+        System.out.println("Идти");
+    }
+
+    public void sit() {
+        System.out.println("Cидеть");
+    }
+
+    public void run() {
+        System.out.println("Бежать");
+    }
+
+    public void talk() {
+        System.out.println("Говорить");
+    }
+
+    public void learnJava() {
+        System.out.println("Учить Java");
+    }
+}
+
